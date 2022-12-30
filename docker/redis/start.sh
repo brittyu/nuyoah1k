4622a25bf7f076d889fad7c8374eee3f253c3aeb
brittyu <yu.britt@gmail.com>
2022-12-30 19:46:56 +0800

    [feature] add protobuf support and update mysql version

--- docker/redis/start.sh
+++ docker/redis/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-redis-server /usr/local/etc/redis/redis.conf &
-redis-server  /usr/local/etc/redis/redis-6378.conf
+redis-server /usr/local/etc/redis/redis-6379.conf &
+redis-server  /usr/local/etc/redis/redis-6378.conf &
+redis-server  /usr/local/etc/redis/redis-6377.conf
